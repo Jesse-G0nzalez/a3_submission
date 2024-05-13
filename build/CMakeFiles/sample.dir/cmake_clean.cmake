39f5835e7aa251a9bb0a72cc754ad64b59d8d4be
Jesse Gonzalez <jesse.gonzalez@student.uts.edu.au>
2024-05-13 11:55:59 +1000

    unsure

---
+++ build/CMakeFiles/sample.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sample.dir/src/main.cpp.o"
+  "CMakeFiles/sample.dir/src/main.cpp.o.d"
+  "CMakeFiles/sample.dir/src/sample.cpp.o"
+  "CMakeFiles/sample.dir/src/sample.cpp.o.d"
+  "sample"
+  "sample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /opt/ros/humble/src/gtest_vendor
+# Build directory: /home/jesse/ros2_ws/src/a3_submission/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
